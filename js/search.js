b8ee5ffaa31fb59c3f147270074e898b0903ee67
Hein Coetzee <heincoetzee11@gmail.com>
2022-05-29 16:33:21 +0200

    Fix searchbar not working on profile page
    Problem: After the user switches to another section on the profile page,
    the search bar stopped working
    Why: The search code only set the confessions from the initial input, because
    I initially wrote the code for the home page and I thought that only
    setting the confessions once would be more time efficient.
    Fix: Remove the initial flag check from search.js

--- js/search.js
+++ js/search.js
@@ -4,11 +4,7 @@ const searchContainer = document.querySelector("#searchbar-wrapper");
 const searchInput = document.querySelector("#search-input");
 const searchBar = new SearchBar(searchContainer);
 
-let initial = true;
 searchInput.addEventListener("input", () => {
-    if (initial) {
-        searchBar.confessions = document.querySelectorAll(".confession-card");
-        initial = false;
-    }
+    searchBar.confessions = document.querySelectorAll(".confession-card");
     searchBar.search();
 });
